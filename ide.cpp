d9986ed9df602b1920cb771d2c5f86e674d4bdd8
YuehunGege <1219253738@qq.com>
2019-09-09 01:18:05 +0800

    增加括号匹配、显示选中行号、自动缩进、控制行号背景色功能

--- ide.cpp
+++ ide.cpp
@@ -3,7 +3,7 @@
 //QsciScintilla作为QWidget的控件，需要添加该控件的头文件
 #include <Qsci/qsciscintilla.h>
 //以python语法作为例子，该语法分析器的头文件
-#include <Qsci/qscilexerpython.h>
+#include <Qsci/qscilexercpp.h>
 //设置代码提示功能，依靠QsciAPIs类实现
 #include <Qsci/qsciapis.h>
 
@@ -31,11 +31,11 @@ IDE::IDE(QWidget *parent)
     QsciScintilla *editor=new QsciScintilla(this);
     setCentralWidget(editor);
     //设置语法
-    QsciLexerPython *textLexer = new QsciLexerPython;//创建一个词法分析器
+    QsciLexerCPP *textLexer = new QsciLexerCPP;//创建一个词法分析器
     editor->setLexer(textLexer);//给QsciScintilla设置词法分析器
     editor->setMarginType(0,QsciScintilla::NumberMargin);//设置编号为0的页边显示行号。
     editor->setMarginLineNumbers(0,true);//对该页边启用行号
-    editor->setMarginWidth(0,15);//设置页边宽度
+    editor->setMarginWidth(0,50);//设置页边宽度
     //代码提示
     QsciAPIs *apis = new QsciAPIs(textLexer);
     apis->add(QString("import"));
@@ -43,6 +43,14 @@ IDE::IDE(QWidget *parent)
     editor->setAutoCompletionSource(QsciScintilla::AcsAll);   //设置源，自动补全所有地方出现的
     editor->setAutoCompletionCaseSensitivity(true);   //设置自动补全大小写敏感
     editor->setAutoCompletionThreshold(1);    //设置每输入一个字符就会出现自动补全的提示
+    //设置自动缩进
+    editor->setAutoIndent(true);
+    //显示选中行号
+    editor->setCaretLineVisible(true);
+    editor->setCaretLineBackgroundColor(Qt::lightGray);
+    //显示行号背景颜色
+    editor->setMarginsBackgroundColor(Qt::red);
+    editor->setBraceMatching(QsciScintilla::SloppyBraceMatch);  //括号匹配
     editor->SendScintilla(QsciScintilla::SCI_SETCODEPAGE,QsciScintilla::SC_CP_UTF8);//设置编码为UTF-8
 }
 
